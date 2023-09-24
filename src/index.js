11a2e2f11e650f133e3bc2fd00301457d2d843b9
Budianto IP <budiantoip@gmail.com>
2023-09-25 05:04:25 +0700

    refactor

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import {Router, listen} from 'worktop';
 import faunadb from 'faunadb';
 import {getProduct} from './products/get.js'
-import {addProduct} from './products/add.js'
-import {addQuantity} from './products/addQuantity.js';
-import { deleteProduct } from './products/delete.js';
+import {addProduct,addQuantity} from './products/add.js'
+import {deleteProduct} from './products/delete.js';
 
 const router = new Router();
 
 const faunaClient = new faunadb.Client({
   secret: FAUNA_SECRET
 });
-
-const {Create, Collection, Match, Index, Get, Ref, Paginate, Sum, Delete, Add, Select, Let, Var, Update} = faunadb.query;
 
 router.add('GET', '/', async (request, response) => {
   response.send(200, 'hello world');
--- src/products/add.js
+++ src/products/add.js
@@ -30,3 +30,38 @@ export async function addProduct(request, response) {
 	  response.send(faunaError.status, faunaError);
 	}
 }
+
+export async function addQuantity(request, response) {
+
+	try {
+	  const productId = request.params.productId;
+	  const {quantity} = await request.body();
+  
+	  const result = await faunaClient.query(
+		Let(
+		  {
+			productRef: Ref(Collection('Products'), productId),
+			productDocument: Get(Var('productRef')),
+			currentQuantity: Select(['data', 'quantity'], Var('productDocument'))
+		  },
+		  Update(
+			Var('productRef'),
+			{
+			  data: {
+				quantity: Add(
+				  Var('currentQuantity'),
+				  quantity
+				)
+			  }
+			}
+		  )
+		)
+	  );
+  
+	  response.send(200, result);
+  
+	} catch (error) {
+	  const faunaError = getFaunaError(error);
+	  response.send(faunaError.status, faunaError);
+	}
+}
